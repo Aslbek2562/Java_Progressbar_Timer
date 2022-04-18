6b9b021996189093b42a9b2e89cb4542efd330d2
Fourcade7 <fourcadeaminov7@gmail.com>
2022-04-18 05:58:51 +0500

    Initial commit

---
+++ app/src/main/java/com/fourcade7/java_progressbar_style_timer/MainActivity.java
@@ -0,0 +1,41 @@
+package com.fourcade7.java_progressbar_style_timer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.os.CountDownTimer;
+
+import com.fourcade7.java_progressbar_style_timer.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+    ActivityMainBinding binding;
+    CountDownTimer cTimer = null;
+    long a=0;
+    int t=0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding=ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.button1.setOnClickListener(view -> {
+            cTimer = new CountDownTimer(5000, 100) {
+                public void onTick(long millisUntilFinished) {
+                    t++;
+                    binding.textview1.setText(t+" %");
+                    binding.progressbar1.setProgress(t);
+                }
+                public void onFinish() {
+                }
+            };
+            cTimer.start();
+
+        });
+        binding.button3.setOnClickListener(view -> {
+            cTimer.cancel();
+            binding.textview1.setText("00:00");
+            t=0;
+        });
+    }
+}
